e0b752a9ae6e1415b7436af7bbf871818b9a4659
EmantheCoder <56139812+EmmanuelIdeho@users.noreply.github.com>
2025-03-06 19:37:17 -0500

    Uploads new backend

---
+++ doc/sprint0/backend/meetmerge/meetmerge/src/main/java/com/meetmerge/MeetmergeApplication.java
@@ -0,0 +1,13 @@
+package com.meetmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetmergeApplication.class, args);
+	}
+
+}
