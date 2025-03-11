289d1677b7a9098d02bd1592eda9d7d388d50757
EmantheCoder <ohis98@gmail.com>
2025-03-11 02:11:51 -0400

    Include new calendly component in main app

--- doc/sprint0/frontend/my-react-app/src/App.jsx
+++ doc/sprint0/frontend/my-react-app/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import { testBackend, responseData} from './api'
 import axios from "axios";
 import './App.css'
+
+import CalendlyInputForm from './CalendlyInput';
 
 function App() {
   const API_URL = "http://localhost:8080/api/meetings";
@@ -21,24 +20,14 @@ const testBackend = async() => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={testBackend}> Press me!</button>
         <div>
           {responseData}
-          </div>
+        </div>
+        <CalendlyInputForm />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
